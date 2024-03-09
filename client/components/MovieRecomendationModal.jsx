757aa7233293bc011834c767cbde306b6aec5557
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-03-09 12:20:34 -0500

    Update rendering so that review and ratins come up after being watched

--- client/components/MovieRecomendationModal.jsx
+++ client/components/MovieRecomendationModal.jsx
@@ -1,22 +1,28 @@
 import {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar, faHeart} from '@fortawesome/free-solid-svg-icons'
+import { faStar, faEye } from '@fortawesome/free-solid-svg-icons'
 import './style.css';
 
 const MovieRecommendationModal = ({ movie }) => {
-  const [isFavorite, setIsFavorite] = useState(false);
+  const [isWatched, setIsWatched] = useState(false);
   const [isHovered, setIsHovered] = useState(false); 
   const [reviews, setReviews] = useState([]);
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
-  const handleFavorite = (event) => {
-    setIsFavorite(!isFavorite);
+  const handleWatched = (event) => {
+    setIsWatched(!isWatched);
+    setReviews([]);
+    setRating(0);
   };
 
   const handleReview = (event) => {
     event.preventDefault();
-    const review = event.target[0].value;
+    const review = event.target[0].value.trim();
+    if (!review) {
+      event.target[0].value = '';
+      return;
+    }
     setReviews([...reviews, review]);
     event.target[0].value = '';
   };
@@ -27,45 +33,53 @@ const MovieRecommendationModal = ({ movie }) => {
   };
 
   return (
-    <div className='flex flex-col items-center p-4 bg-white rounded shadow'>
+    <div className='flex flex-col items-center p-4 bg-gray-800 text-gray-200 rounded shadow'>
+      <div className='flex flex-col pb-5'>
+      <span className="flex justify-center font-bold ml-2 text-lg p-2 text-center">Watched?</span> 
       <FontAwesomeIcon
-        icon={faHeart}
-        className={`text-lg cursor-pointer ${isFavorite || isHovered ? 'text-red-500' : 'text-gray-500'}`}
+        icon={faEye}
+        className={`text-lg cursor-pointer ${isWatched || isHovered ? 'text-red-500' : 'text-gray-500'}`}
         onMouseEnter={() => setIsHovered(true)} 
         onMouseLeave={() => setIsHovered(false)} 
-        onClick={handleFavorite}
+        onClick={handleWatched}
       />
+      </div>
       <h3 className='mt-2 text-xl font-bold'>{movie.title}</h3>
       <img className='mt-2 rounded' src={movie.image} alt={movie.title}/>
-      <form className='mt-2 w-full' onSubmit={handleReview} name='recommendations'>
-        <input className='w-full p-2 border rounded' type="text" placeholder="Add a review" />
-        <button className='mt-2 w-full p-2 hover:bg-blue-700 bg-blue-500 text-white rounded' type='submit'>Submit</button>
-        <p className='mt-2 font-bold'>Reviews:</p>
-        {reviews.length > 0 && <ul className='mt-2'>{reviews.map((review, index) => <li className='flex justify-between mt-1' key={index}>{review} <button className='ml-2 text-red-500' type='button' onClick={() => handleReviewDelete(index)}>X</button></li>)}</ul>}
-      </form>
-      <div className='flex mt-2'>
-        {[...Array(5)].map((star, i) => {
-          const ratingValue = i + 1;
+      
+      {isWatched && (
+  <>
+    <form className='mt-2 w-full' onSubmit={handleReview} name='recommendations'>
+      <input className='w-full p-2 border border-gray-600 rounded bg-gray-700 text-white text-center' type="text" placeholder="Add a review" maxLength="40"/>
+      <button className='mt-2 w-full p-2 hover:bg-red-600 bg-red-500 text-white rounded' type='submit'>Submit</button>
+      <p className='mt-2 font-bold'>Reviews:</p>
+      {reviews.length > 0 && <ul className='mt-2'>{reviews.map((review, index) => <li className='flex justify-between items-center mt-1 border-b p-2 word-break break-all' key={index}>{review} <button className='px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600' type='button' onClick={() => handleReviewDelete(index)}>X</button></li>)}</ul>}
+    </form>
+    <div className='flex mt-2'>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
 
-          return (
-            <label key={i}>
-              <input
-                type="radio"
-                name="rating"
-                value={ratingValue}
-                onClick={() => setRating(ratingValue)}
-                className='sr-only'
-              />
-              <FontAwesomeIcon
-                icon={faStar}
-                className={`mr-1 cursor-pointer ${ratingValue <= (hover || rating) ? "text-yellow-400" : "text-gray-300"}`}
-                onMouseEnter={() => setHover(ratingValue)}
-                onMouseLeave={() => setHover(0)}
-              />
-            </label>
-          );
-        })}
-      </div>
+        return (
+          <label key={i}>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+              className='sr-only'
+            />
+            <FontAwesomeIcon
+              icon={faStar}
+              className={`mr-1 cursor-pointer ${ratingValue <= (hover || rating) ? "text-red-500" : "text-gray-300"}`}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(0)}
+            />
+          </label>
+        );
+      })}
+    </div>
+  </>
+)}
     </div>
   );
 }
